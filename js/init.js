b33a395b438d338b8270cd0769ec9b677a8cf0cc
AJ Almaguer <ajalmaguer@gmail.com>
2016-05-17 17:48:06 -0700

    started from materialize template

---
+++ js/init.js
@@ -0,0 +1,13 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.slider')
+      .slider({
+        height: 300,
+        interval: 1500
+      })
+
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
