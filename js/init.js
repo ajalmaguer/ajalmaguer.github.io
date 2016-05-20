3f13fa5496c3a9b2b389af80ae62824c0a834fcc
AJ Almaguer <ajalmaguer@gmail.com>
2016-05-20 12:05:06 -0700

    added lazy loading of divs

--- js/init.js
+++ js/init.js
@@ -15,5 +15,20 @@ $("#tagline").typed({
   showCursor: false
 })
 
+$("footer").css("opacity", 1)
+
+
+$(window).scroll( function(){
+  /* Check the location of each desired element */
+  $('.project').each( function(i){
+    var top_of_object = $(this).offset().top;
+    var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+    /* If the object is completely visible in the window, fade it in */
+    if( bottom_of_window > top_of_object + 200  ){
+      $(this).animate({'opacity':'1'},500);
+    }
+  });
+});
 
 
